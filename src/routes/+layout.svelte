7f38168d1bc64b115e44349048287adcbcace082
solverbot <kamaljp@gmail.com>
2022-10-25 10:22:35 +0530

    Starting the sales performance dashboard

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
   <!-- svelte-ignore a11y-missing-attribute -->
   <a class="btn btn-ghost normal-case text-xl" href="/">Home</a>	
 	<div class="dropdown">
+	  <label tabindex="0" class="btn btn-ghost normal-case text-xl m-1">DashBoards</label>
+	  <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+		<li><a href="/performanceDB">Sales Performance Dashboard</a></li>
+	  </ul>
+	</div>
+	<div class="dropdown">
 	  <label tabindex="0" class="btn btn-ghost normal-case text-xl m-1">Gallery</label>
 	  <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
 		<li><a href="/sample">Basic Charts</a></li>
