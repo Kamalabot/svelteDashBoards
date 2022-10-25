3cf26800c0d8eb0a685c09a01deb5109466d6268
solverbot <kamaljp@gmail.com>
2022-10-25 11:38:22 +0530

    learning tailwind midway

---
+++ src/routes/performanceDB/+page.svelte
@@ -0,0 +1,35 @@
+
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+<div>
+  <header class="bg-white">
+    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Sale Performance Dashboard</h1>
+    </div>
+  </header>
+</div>
+<div class="flex justify-center place-content-center gap-4 py-6 h-96">
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">Sales $</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>If a dog chews shoes whose shoes does he choose?</p>
+	  </div>
+	</div>
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">Gross Margin%</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>If a dog chews shoes whose shoes does he choose?</p>
+	  </div>
+	</div>
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">ADS $</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>If a dog chews shoes whose shoes does he choose?</p>
+	  </div>
+	</div>
+</div>
+</body>
+</html>
