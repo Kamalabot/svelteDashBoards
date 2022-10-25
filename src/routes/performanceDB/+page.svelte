d325a170ebdd7e59a5ab9a9e7c4cbccbb93cd139
solverbot <kamaljp@gmail.com>
2022-10-25 12:09:54 +0530

    Completed the dashboard Wireframe

--- src/routes/performanceDB/+page.svelte
+++ src/routes/performanceDB/+page.svelte
@@ -8,26 +8,75 @@
     </div>
   </header>
 </div>
-<div class="flex justify-center place-content-center gap-4 py-6 h-96">
+<div class="flex justify-center gap-4 p-6 h-96">
 	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
 	  <div class="card-body">
 		<h2 class="card-title">Sales $</h2>
 		<figure><svg width=250 height=200 class="bg-primary" /></figure>
-		<p>If a dog chews shoes whose shoes does he choose?</p>
+		<p>Sales Performance of the Store</p>
 	  </div>
 	</div>
 	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
 	  <div class="card-body">
 		<h2 class="card-title">Gross Margin%</h2>
 		<figure><svg width=250 height=200 class="bg-primary" /></figure>
-		<p>If a dog chews shoes whose shoes does he choose?</p>
+		<p>Margin Performance of the Store</p>
 	  </div>
 	</div>
 	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
 	  <div class="card-body">
 		<h2 class="card-title">ADS $</h2>
 		<figure><svg width=250 height=200 class="bg-primary" /></figure>
-		<p>If a dog chews shoes whose shoes does he choose?</p>
+		<p>Ads usage of the Store</p>
+	  </div>
+	</div>
+</div>
+<div class="flex justify-center gap-4 p-6 h-96">
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">Sales/ Day</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>Sales Performance of the Store</p>
+	  </div>
+	</div>
+	<div class="w-64 bg-base-100 shadow-xl">  
+		  <div class="stat">
+			<div class="stat-title">Unit/Txn</div>
+			<div class="stat-value text-primary">25.6K</div>
+			<div class="stat-desc">What cost?</div>
+		  </div>
+		  <div class="stat">
+			<div class="stat-title">Sales Units</div>
+			<div class="stat-value text-secondary">2.6M</div>
+			<div class="stat-desc">How Many?</div>
+		  </div>
+		  <div class="stat">
+			<div class="stat-title">Traffic / Day</div>
+			<div class="stat-value text-secondary">2.6M</div>
+			<div class="stat-desc">What Number?</div>
+		  </div>
+	</div>
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">UPT Trend Weekly</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>Ads usage of the Store</p>
+	  </div>
+	</div>
+</div>
+<div class="flex justify-center gap-4 p-6 h-96">
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">Top 5 stores</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>Sales Performance of the Store</p>
+	  </div>
+	</div>
+	<div class="flex-auto card w-96 bg-base-100 shadow-xl">
+	  <div class="card-body">
+		<h2 class="card-title">Store Leader Board</h2>
+		<figure><svg width=250 height=200 class="bg-primary" /></figure>
+		<p>Table containing Sales, GMargin, ADs$, UPT, Sales/Unit, Traffic, Trend </p>
 	  </div>
 	</div>
 </div>
