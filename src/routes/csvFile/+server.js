9c7cfab096d111bd81379d79c9d61975151e35f5
solverbot <kamaljp@gmail.com>
2022-10-29 12:59:22 +0530

    Learnt how the postgres library queries the database and wrote the csvFile end point for get and posting the table name to the server

--- src/routes/csvFile/+server.js
+++ src/routes/csvFile/+server.js
@@ -6,6 +6,20 @@ export const GET = async({request, locals})=>{
     const {sql} = locals;
      
     const irisData = await sql`SELECT * FROM iris`
-    console.log(irisData,'API reply')
-    return new Response(JSON.stringify({message:'Recd Data', data:irisData}),{status:200})
+	//const custAcq = await sql`SELECT * FROM custacq`
+	//const monthlyTgts = await sql`SELECT * FROM monthlytgt`
+	//const visHeads = await sql`SELECT * FROM vizhead` 
+	//const fmcgCompany = await sql`SELECT * FROM fmcgindia` 
+    const tableData = {irisData} 
+    console.log(tableData,'API reply')
+    return new Response(JSON.stringify(tableData),{status:200})
 }
+
+export const POST = async({request, locals})=>{
+	const {sql} = locals;	
+	console.log("Requesting specific tables from the database!!!");
+    const body = await request.json();
+	const tableName = body.tableName;
+	const yourTable = await sql`SELECT * FROM ${ sql(tableName) }`
+	return new Response(JSON.stringify({yourTable}));
+} 
